39e19291084fd333c8de6a40ff36d45e15bffca0
abhishek <98287339+WebDevOliva@users.noreply.github.com>
2023-09-18 00:19:28 +0530

    artist grid view created

--- components/component/artist/MusicVideoContainer/MusicVideo_Rows.js
+++ components/component/artist/MusicVideoContainer/MusicVideo_Rows.js
@@ -1,30 +1,46 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FiMoreVertical } from "react-icons/fi";
+import formateNumber from 'number_formatter'
+import {AiOutlineHeart,AiFillHeart} from 'react-icons/ai'
 
-const MusicVideo_Rows = ({ music }) => {
+
+const MusicVideo_Rows = ({ music, artistInformation }) => {
+    const [videoDuration,setVedioDuration] = useState(`00:00`)
+
   return (
-    <div className={`w-full cursor-pointer flex justify-between items-center mb-5`}>
-      {/* thumbnail */}
-      <div className="relative w-[160px] h-[100px] bg-[#000] rounded-xl overflow-hidden mr-2">
-        <Image
-          src={music?.poster?.thumbnail}
-          alt="music video thumbnail"
-          width={500}
-          height={500}
-          className="relative w-full h-full object-cover filter brightness-[0.8]"
-        />
+    <div
+      className={`w-full cursor-pointer flex justify-start items-center mb-2 hover:bg-[#222] hover:bg-opacity-[0.5] py-4 px-5 rounded-lg gap-10`}
+    >
+      <div className="flex justify-start items-center flex-[50%]">
+        {/* thumbnail */}
+        <div className="relative w-[200px] h-[75px] bg-[#000] rounded-xl overflow-hidden mr-2">
+          <Image
+            src={music?.poster?.thumbnail}
+            alt="music video thumbnail"
+            width={500}
+            height={500}
+            className="relative w-full h-full object-cover filter brightness-[0.8]"
+          />
+        </div>
+        {/* information */}
+        <div className="w-full  flex justify-start items-start p-2">
+          <div className="">
+            <article className="text-[#ccc] max-w-[80%] text-sm">
+              {music?.music_v}
+            </article>
+          </div>
+        </div>
       </div>
-      {/* information */}
-      <div className="w-full flex justify-start items-start p-2">
-        <div className="flex-[1]">
-          <article className="text-[#ccc] max-w-[80%] text-sm">
-            {music?.music_v}
-          </article>
-        </div>
-        <div className="py-2">
-          <FiMoreVertical className="text-[#ccc]" />
-        </div>
+
+
+        <div className="text-xs text-off-white flex justify-center items-center flex-[25%]">{formateNumber(artistInformation?.monthly_viewvers)}</div>
+
+
+      <div className="py-2 flex-[25%] flex justify-end items-center gap-5">
+        {false ? <AiFillHeart className="text-xl text-green" /> : <AiOutlineHeart  className="text-xl text-green"/>}
+        <article className="text-off-white text-xs">00:00</article>
+        <FiMoreVertical className="text-[#ccc]" />
       </div>
     </div>
   );
--- components/component/artist/OfficialMV.js
+++ components/component/artist/OfficialMV.js
@@ -1,33 +1,41 @@
-import React from 'react'
-import MusicVideo_Gallery from './MusicVideoContainer/MusicVideo_Gallery'
-import MusicVideo_Rows from './MusicVideoContainer/MusicVideo_Rows'
+import React from "react";
+import MusicVideo_Gallery from "./MusicVideoContainer/MusicVideo_Gallery";
+import MusicVideo_Rows from "./MusicVideoContainer/MusicVideo_Rows";
 
-const OfficialMV = ({artist_content,tab_view}) => {
-  return (
-    <div className={`relative w-full h-auto grid ${tab_view === 1 ? 'grid-cols-4 gap-5' : 'grid-cols-1' }`}>
-        {
-            tab_view === 1 ? 
-            <>
-                {
-                    artist_content?.videos?.map((mv,idx) => {
-                        return (
-                            <MusicVideo_Gallery music={mv} key={idx}/>
-                        )
-                    })
-                }
-            </> :
-            <>
-            {
-                    artist_content?.videos?.map((mv,idx) => {
-                        return (
-                            <MusicVideo_Rows music={mv} key={idx}/>
-                        )
-                    })
-                }
-            </>
-        }
-    </div>
-  )
-}
+const OfficialMV = ({ artist_content, tab_view }) => {
+  return artist_content?.video?.length !== 0 ? (
+    <>
+      <div
+        className={`relative w-full h-auto grid ${
+          tab_view === 1 ? "grid-cols-4 gap-5" : "grid-cols-1"
+        }`}
+      >
+        {tab_view === 1 ? (
+          <>
+            {artist_content?.videos?.map((mv, idx) => {
+              return <MusicVideo_Gallery music={mv} key={idx} />;
+            })}
+          </>
+        ) : (
+          <>
+            {artist_content?.videos?.map((mv, idx) => {
+              return (
+                <MusicVideo_Rows
+                  music={mv}
+                  key={idx}
+                  artistInformation={artist_content}
+                />
+              );
+            })}
+          </>
+        )}
+      </div>
+    </>
+  ) : (
+    <>
+        <article className="text-off-white text-3xl text-center">`No Music Videos Have Been Uploded By {artist_content?.artist_name}`</article>
+    </>
+  );
+};
 
-export default OfficialMV
+export default OfficialMV;
