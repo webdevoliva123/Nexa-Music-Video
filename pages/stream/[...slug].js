d2d9281368f7d2dbfb2543bc62bce9efbcd7693e
abhishek <98287339+WebDevOliva@users.noreply.github.com>
2023-10-03 02:27:58 +0530

    music player created

--- pages/stream/[...slug].js
+++ pages/stream/[...slug].js
@@ -1,11 +1,104 @@
 
+import TabContent from '@/components/component/artist/TabContent';
 import MusicVideo_Player from '@/utils/MusicVideo_Player'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { CiGrid2H, CiGrid41 } from "react-icons/ci";
+import famousArtist from "@/json/famous-artist.json";
 
 const Player = () => {
+  const tabs = [
+    {
+      id: "official-mv",
+      name: "Official MV",
+    },
+    {
+      id: "albums-by-artist",
+      name: "Albums",
+    },
+    {
+      id: "playlist-by-artist",
+      name: "Playlist",
+    },
+    {
+      id: "other-videos",
+      name: "Other Videos",
+    },
+  ];
+  const [currentArtist, setCurrentArtist] = useState();
+  const [currentTab, setCurrentTab] = useState("official-mv");
+  const [currentView, setCurrentView] = useState(1);
+
+  const followed = true;
+
+
+  useEffect(() => {
+    setCurrentArtist(famousArtist.find((artist) => artist.id === 'iu'));
+  }, []);
   return (
-    <div className='relative w-full h-[75vh]'>
+    <div>
+      <div className='relative w-full h-[75vh] '>
         <MusicVideo_Player videoSrc={'https://res.cloudinary.com/dkz1pnb2b/video/upload/v1696260069/2020_FESTA_BTS_%E1%84%87%E1%85%A1%E1%86%BC%E1%84%90%E1%85%A1%E1%86%AB%E1%84%89%E1%85%A9%E1%84%82%E1%85%A7%E1%86%AB%E1%84%83%E1%85%A1%E1%86%AB_We_are_Bulletproof_the_Eternal_MV_2020BTSFESTA_lgneob.mp4'} />
+        
+    </div>
+    <div className="w-full mb-16 px-10">
+    <article className="text-[#fff] text-3xl font font-semibold mb-8 cursor-pointer">
+      Discover All.
+    </article>
+    {/* Tabs */}
+    <div className="w-full border-b-[0.1px] border-off-white pb-5  flex justify-between items-center mb-8">
+      {/* left section */}
+      <div className="flex justify-start items-center gap-4">
+        {tabs.map((tab, idx) => {
+          return (
+            <div
+              key={idx}
+              className="py-2 px-4 bg-[#222] rounded-full"
+              onClick={() => setCurrentTab(tab.id)}
+            >
+              <article
+                className={`text-xs ${
+                  currentTab === tab.id
+                    ? "text-green"
+                    : "text-off-white"
+                } cursor-pointer`}
+              >
+                {tab.name}
+              </article>
+            </div>
+          );
+        })}
+      </div>
+      {/* rigth section */}
+      <div className="flex justify-end items-center gap-3">
+        <article className="text-[10px] text-off-white cursor-pointer">
+          SHOW MORE
+        </article>
+        {currentTab === "official-mv" && (
+          <div className="flex justify-end items-center gap-2">
+            <CiGrid41
+              className={`${
+                currentView === 1 ? "text-green" : "text-off-white"
+              } text-xl cursor-pointer`}
+              onClick={() => setCurrentView(1)}
+            />
+            <CiGrid2H
+              className={`${
+                currentView === 2 ? "text-green" : "text-off-white"
+              } text-xl cursor-pointer`}
+              onClick={() => setCurrentView(2)}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+
+    {/* tab Conetnt */}
+    <TabContent
+      current_tab={currentTab}
+      artist_content={currentArtist}
+      tab_view={currentView}
+    />
+  </div>
     </div>
   )
 }
--- utils/MusicVideo_Player.js
+++ utils/MusicVideo_Player.js
@@ -1,79 +1,95 @@
 import Image from "next/image";
 import React, { useState, useRef, useEffect } from "react";
-import {TbPlayerTrackPrev, TbPlayerTrackNext, TbPlayerPauseFilled, TbPlayerPlayFilled,TbRepeat,TbArrowsShuffle} from 'react-icons/tb'
+import {
+  TbPlayerTrackPrev,
+  TbPlayerTrackNext,
+  TbPlayerPauseFilled,
+  TbPlayerPlayFilled,
+  TbRepeat,
+  TbArrowsShuffle,
+} from "react-icons/tb";
+import {
+  BsFullscreen,
+  BsFullscreenExit,
+  BsFillVolumeUpFill,
+  BsFillVolumeOffFill,
+  BsFillVolumeMuteFill,
+  BsFillVolumeDownFill,
+  BsVolumeDownFill
+} from "react-icons/bs";
+import {
+  AiTwotoneSetting
+} from "react-icons/ai";
 
 const MusicVideoPlayer = ({ videoSrc }) => {
-  
   const [isPlaying, setIsPlaying] = useState(false);
   const [totalDuration, setTotalDuration] = useState(0);
-  const [currentTime,setCurrentTime] = useState(0);
-
+  const [currentTime, setCurrentTime] = useState(0);
 
   const videoRef = useRef();
-  const progressBar = useRef()
-  const animationRef = useRef()
+  const progressBar = useRef();
+  const animationRef = useRef();
 
   useEffect(() => {
     const videoElement = videoRef.current;
-    const progressElement = progressBar.current
+    const progressElement = progressBar.current;
 
-    const duration = Math.floor(videoElement?.duration)
-    progressElement.max = duration
+    const duration = Math.floor(videoElement?.duration);
+    progressElement.max = duration;
 
-    setTotalDuration(duration)
-  }, [videoRef?.current?.loademetadata,videoRef?.current?.readyState]);
+    setTotalDuration(duration);
+  }, [videoRef?.current?.loademetadata, videoRef?.current?.readyState]);
 
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = Math.floor(seconds % 60);
-    return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+    return `${String(minutes).padStart(2, "0")}:${String(
+      remainingSeconds
+    ).padStart(2, "0")}`;
   };
 
- 
-
   const handlePlayPause = () => {
-    const prevVal = isPlaying
+    const prevVal = isPlaying;
     setIsPlaying(!prevVal);
     if (!prevVal) {
       videoRef.current.play();
-      animationRef.current = requestAnimationFrame(whilePlaying)
+      animationRef.current = requestAnimationFrame(whilePlaying);
     } else {
       videoRef?.current?.pause();
-      cancelAnimationFrame(animationRef.current)
+      cancelAnimationFrame(animationRef.current);
     }
-  }
+  };
 
   const whilePlaying = () => {
-    progressBar.current.value = videoRef.current.currentTime; 
-    progressBar.current.style.setProperty('--seek-before-width',`${progressBar.current.value / totalDuration * 100 }%`)
-    setCurrentTime(progressBar?.current.value)
+    progressBar.current.value = videoRef.current.currentTime;
+    progressBar.current.style.setProperty(
+      "--seek-before-width",
+      `${(progressBar.current.value / totalDuration) * 100}%`
+    );
+    setCurrentTime(progressBar?.current.value);
 
     animationRef.current = requestAnimationFrame(whilePlaying);
-  }
+  };
 
   const progessBarHandler = () => {
-    videoRef.current.currentTime = progressBar?.current?.value
-    progressBar.current.style.setProperty('--seek-before-width',`${progressBar.current.value / totalDuration * 100 }%`)
-    setCurrentTime(progressBar?.current.value)
-  }
-  
-
- 
+    videoRef.current.currentTime = progressBar?.current?.value;
+    progressBar.current.style.setProperty(
+      "--seek-before-width",
+      `${(progressBar.current.value / totalDuration) * 100}%`
+    );
+    setCurrentTime(progressBar?.current.value);
+  };
 
   const backThirty = () => {
     progressBar.current.value = Number(progressBar.current.value - 30);
     progessBarHandler();
-  }
+  };
 
   const forwardThirty = () => {
     progressBar.current.value = Number(progressBar.current.value + 30);
     progessBarHandler();
-  }
+  };
 
-
-
- 
- 
   return (
     <>
       <div className="relative w-full h-full z-0">
@@ -82,58 +98,111 @@ const MusicVideoPlayer = ({ videoSrc }) => {
           src={videoSrc}
           className="relative w-full h-full object-cover z-0 filter brightness-[0.8]"
           preload="metadata"
-
         />
       </div>
       <div
-          style={{
-            position: "absolute",
-            top: "0",
-            left: "0",
-            width: "100%",
-            height: "100%",
-            backgroundImage: "linear-gradient(transparent, #111 90%)",
-            zIndex: 0,
-          }}
-        ></div>
-      <div
-        className="video_player"
-      >
-       <div className="flex-[0.2] w-full flex justify-start items-end">
-        
-      </div> 
-       <div className="flex-[0.6] w-full  flex flex-col justify-center items-center">
+        style={{
+          position: "absolute",
+          top: "0",
+          left: "0",
+          width: "100%",
+          height: "100%",
+          backgroundImage: "linear-gradient(transparent, #111 90%)",
+          zIndex: 0,
+        }}
+      ></div>
+      <div className="video_player">
+        <div className="flex-[0.2] w-full flex justify-start items-end">
+          <div className="relative player_thumbnail">
+            <Image
+              src={
+                "https://res.cloudinary.com/dkz1pnb2b/image/upload/v1696276849/artworks-PkAYHiyqVKRO180Y-Ck7Elg-t500x500_sxiytd.jpg"
+              }
+              alt="thumbnail"
+              width={500}
+              height={500}
+              className="relative w-full h-full object-cover"
+            />
+            <div className={`msuic_playing_indicatior ${isPlaying && 'active'}`}>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-[0.6] w-full  flex flex-col justify-center items-center">
           {/* top */}
           <div className="flex justify-center items-center gap-2 mb-4">
-          <TbArrowsShuffle size={'15'} className="text-off-white text-[20px] "/>
-            <TbPlayerTrackPrev size={'20'} className="text-[#fff] text-[20px] cursor-pointer" onClick={backThirty}/>
+            <TbArrowsShuffle
+              size={"15"}
+              className="text-off-white text-[20px] "
+            />
+            <TbPlayerTrackPrev
+              size={"20"}
+              className="text-[#fff] text-[20px] cursor-pointer"
+              onClick={backThirty}
+            />
             {/*  */}
-              {!isPlaying ? <TbPlayerPlayFilled size={'40'} className="text-green text-[20px] cursor-pointer" onClick={handlePlayPause}/> : <TbPlayerPauseFilled size={'40'} className="text-white text-[20px] cursor-pointer" onClick={handlePlayPause}/>  }
+            {!isPlaying ? (
+              <TbPlayerPlayFilled
+                size={"40"}
+                className="text-green text-[20px] cursor-pointer"
+                onClick={handlePlayPause}
+              />
+            ) : (
+              <TbPlayerPauseFilled
+                size={"40"}
+                className="text-white text-[20px] cursor-pointer"
+                onClick={handlePlayPause}
+              />
+            )}
             {/*  */}
-            <TbPlayerTrackNext size={'20'} className="text-[#fff] text-[20px] cursor-pointer " onClick={forwardThirty}/>
-            <TbRepeat size={'15'} className="text-off-white text-[20px] ml-5"/>
+            <TbPlayerTrackNext
+              size={"20"}
+              className="text-[#fff] text-[20px] cursor-pointer "
+              onClick={forwardThirty}
+            />
+            <TbRepeat size={"15"} className="text-off-white text-[20px] ml-5" />
           </div>
           {/* middle */}
           <div className="player_idicator mb-5">
             {/* current time */}
-            <article className="player_current_time">{formatTime(currentTime) || '00:00'}</article>
-            <input ref={progressBar} type="range" className="player_idicator_position" onChange={progessBarHandler}  defaultValue={0}/>
+            <article className="player_current_time">
+              {formatTime(currentTime) || "00:00"}
+            </article>
+            <input
+              ref={progressBar}
+              type="range"
+              className="player_idicator_position"
+              onChange={progessBarHandler}
+              defaultValue={0}
+            />
             {/* total duration */}
-            <article className="player_total_duration">{(totalDuration && !isNaN(totalDuration)) && formatTime(totalDuration) || '00:00'}</article>
+            <article className="player_total_duration">
+              {(totalDuration &&
+                !isNaN(totalDuration) &&
+                formatTime(totalDuration)) ||
+                "00:00"}
+            </article>
           </div>
           {/* bottom */}
           <div className="flex flex-col justify-center items-center">
-            <article className="text-off-white text-sm text-center"> BTS (방탄소년단) 'We are Bulletproof : the Eternal' MV</article>
+            <article className="text-off-white text-sm text-center">
+              {" "}
+              BTS (방탄소년단) 'We are Bulletproof : the Eternal' MV
+            </article>
           </div>
-       </div>
-       <div className="flex-[0.2] w-full ">
-
-       </div>
+        </div>
+        <div className="flex-[0.2] w-full">
+          <div className="other_options_container">
+          <BsFillVolumeUpFill  size={'20'} className="text-white hover:text-green cursor-pointer" />
+          <AiTwotoneSetting  size={'20'} className="text-white hover:text-green cursor-pointer" />
+          <BsFullscreen  size={'20'} className="text-white hover:text-green cursor-pointer" />
+          </div>
+        </div>
       </div>
     </>
   );
 };
-
-
 
 export default MusicVideoPlayer;
